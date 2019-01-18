8ceba8d664131c38c2e1892b136e5524bfb0c077
phoebekaranja <phoebemurish@gmail.com>
2019-01-18 13:50:58 +0300

    modified an app.module.ts

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuotesFormComponent } from './quotes-form/quotes-form.component';
-
 
 @NgModule({
   declarations: [
@@ -14,11 +13,10 @@ import { QuotesFormComponent } from './quotes-form/quotes-form.component';
     QuotesComponent,
     QuoteDetailsComponent,
     QuotesFormComponent
-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
