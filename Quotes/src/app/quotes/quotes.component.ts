c21b632a409e03c5e6186d975cef4d2ef64c7213
phoebekaranja <phoebemurish@gmail.com>
2019-01-18 11:45:06 +0300

    making some commits in components files

--- Quotes/src/app/quotes/quotes.component.ts
+++ Quotes/src/app/quotes/quotes.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
-})
+});
 export class QuotesComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+  quoted = [
+    new Quote(1, `Leo Tolstoy`, `Everyone thinks of changing the world but no one thinks of changing himself`,  'Tarick', 0, 0, 0),
+    new Quote(2,  'Nelson Mandela ', 'Education is the most powerful weapon which you can use to change the world.',  'Joe', 0, 0, 0),
+    new Quote(3,  'Mary Wollstonecraft Shelley, Frankenstein', 'Nothing is so painful to the human mind as a great and sudden change.',  'Donald', 0, 0, 0),
+    new Quote(4,  'Jay Asher', 'You cannot stop the future,You cannot rewind the past. The only way to learn the secret ...is to press play. ',  'Peter', 0, 0, 0),
+    new Quote(5,  'Taylor Swift', 'This is a new year. A new beginning. And things will change.',  'Wanyeki', 0, 0, 0),
+    new Quote(6,  'Neil Gaiman', 'Google can bring you back 100,000 answers. A librarian can bring you back the right one.',  'Abdifatah', 0, 0, 0),
+  ]
