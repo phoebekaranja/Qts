e8e42f7e847715be7024d70f3cc09ab54cc73b65
phoebekaranja <phoebemurish@gmail.com>
2019-01-18 15:15:27 +0300

    make all changes

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuoteDetailsComponent,
-    QuotesFormComponent
+    QuotesFormComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule,
