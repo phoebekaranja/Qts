20b14a463bb60146773179ffafea93fdbc29eaaa
phoebekaranja <phoebemurish@gmail.com>
2019-01-18 18:15:57 +0300

    all changes

--- Quotes/src/app/quote-details/quote-details.component.ts
+++ Quotes/src/app/quote-details/quote-details.component.ts
@@ -5,7 +5,7 @@ import { Quote } from '../quote';
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
-}
+})
 export class QuoteDetailsComponent implements OnInit {
 
   @Input() quote: Quote;
