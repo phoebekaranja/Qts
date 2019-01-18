cbe55660c0cb52651a20c523a08f2b4c4085cf3b
phoebekaranja <phoebemurish@gmail.com>
2019-01-18 11:41:58 +0300

    entrance to commits

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
